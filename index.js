d73731446034643c6238a6a1243d6c6d3ab6f163
Sui Gn <natsynapses@gmail.com>
2024-04-25 12:15:10 -0500

    .
    .

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ this.env/index.js
 🆂🆄🅸🅶🅽                                                                                                            
 --------------------------------
 For more information, visit: https://neurons.me*/
-console.log("this.env loaded;")
+export default class thisEnv {
+    constructor() {
+        this.env = process.env;
+    }
+    getEnv() {
+        return this.env;
+    }
+    getEnvVar(envVar) {
+        return this.env[envVar];
+    }
+    setEnvVar(envVar, value) {
+        this.env[envVar] = value;
+    }
+    deleteEnvVar(envVar) {
+        delete this.env[envVar];
+    }
+}      
+
+console.log("this.env loaded;");
+
+//export default thisEnv; //uncomment this line to use this module in other modules.
